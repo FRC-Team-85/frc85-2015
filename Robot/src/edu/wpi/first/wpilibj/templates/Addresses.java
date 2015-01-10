d948a10f666712efb425edbd9e0aec5d4b7802a9
Matt Michielsen <matt@michielsen.us>
2015-01-10 09:41:21 -0500

    Mecanum drive base code

---
+++ Robot/src/edu/wpi/first/wpilibj/templates/Addresses.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ *
+ * @author mattmichielsen
+ */
+public static class Addresses {
+    public static final int LEFT_FRONT_MOTOR = 1;
+    public static final int LEFT_REAR_MOTOR = 2;
+    public static final int RIGHT_FRONT_MOTOR = 3;
+    public static final int RIGHT_REAR_MOTOR = 4;
+    
+    public static final int DRIVE_CONTROLLER = 1;
+}
